d53797842a09ea864d94ef74ec353d4ab4bf72f1
Gowtham Nandipati <gowthamnandipati7@gmail.com>
2025-01-01 15:09:04 +0530

    removed extra line

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ document.getElementById("submit").addEventListener("click", function () {
         let user = users.find(user => user.name === username && user.password === password);
 
         if (user) {
-        
             window.location.href = "home.html";
         } else {
             alert("Username or password is incorrect!");
