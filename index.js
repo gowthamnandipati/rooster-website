407b130594d799e4b7ee49c57d09bab2a8776410
Gowtham Nandipati <gowthamnandipati7@gmail.com>
2025-01-01 15:05:21 +0530

    intiial push

---
+++ index.js
@@ -0,0 +1,25 @@
+
+document.getElementById("submit").addEventListener("click", function () {
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+
+    if (username && password) {
+      
+        let users = JSON.parse(localStorage.getItem("users")) || [];
+
+      
+        let user = users.find(user => user.name === username && user.password === password);
+
+        if (user) {
+        
+            window.location.href = "home.html";
+        } else {
+            alert("Username or password is incorrect!");
+        }
+    } else {
+        alert("Enter all the credentials!");
+    }
+});
+
+
+
---
+++ register.js
@@ -0,0 +1,31 @@
+document.getElementById("submit").addEventListener("click",function(){
+
+    let userName = document.getElementById("username").value
+    let Number = document.getElementById("number").value
+    let Password = document.getElementById("password").value
+
+    if (userName && Number && Password){ 
+
+         let users = JSON.parse(localStorage.getItem("users")) || [];
+
+        let userexists = users.some(user => user.name === username)
+        
+        if (userexists){
+            alert("user already exists ! ")
+        }else{
+            let userdetails ={name:userName,number:Number,password:Password}
+            users.push(userdetails)
+            localStorage.setItem("users",JSON.stringify(users))
+            alert("user registration is sucessful")
+            window.location.href="index.html"
+        }
+    
+    }else{
+        alert("enter credentials!")
+    }
+        
+
+       
+    
+
+})
